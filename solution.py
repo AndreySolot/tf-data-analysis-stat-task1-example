93edbedb6f88f8d9b97667af1d8a32f743dd58ac
Andrey Solot <114979763+AndreySolot@users.noreply.github.com>
2023-04-04 12:58:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 163596104 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return np.log(np.exp(sample - 723).mean()  # Ваш ответ# Ваш ответ
